d805dbf6f82e527fddb1bd9917b2355ab5157afb
RicArora1991 <richaaeri9988@gmail.com>
2024-09-09 16:11:22 +0530

    initial commit2

--- screens/VegetablesScreen.js
+++ screens/VegetablesScreen.js
@@ -16,21 +16,7 @@ export default function VegetablesScreen() {
   
     return (
       <ScrollView style={styles.container}>
-        <Text style={styles.title}>Fresh vegetables directy from farmers land</Text>
-        {vegetables.map((vegetable, index) => (
-          <View key={index} style={styles.itemContainer}>
-            <Image source={{ uri: vegetable.image }} style={styles.image} />
-            <View style={styles.details}>
-              <Text style={styles.name}>{vegetable.name}</Text>
-              <Text style={styles.price}>{vegetable.price}</Text>
-              <TouchableOpacity style={styles.button} onPress={() => alert(`${vegetable.name} added to cart`)}>
-            <Text style={styles.buttonText}>Add To Cart</Text>
-          </TouchableOpacity>
-            </View>
-           
-           
-          </View>
-        ))}
+        
       </ScrollView>
     );
   }
